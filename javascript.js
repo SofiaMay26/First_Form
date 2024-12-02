7a45159d7d47b31c3091b641cd9a9ad769c3d87a
SofiaMay26 <tempsofimae@gmail.com>
2024-12-02 16:31:54 +0800

    added another index1 and js 1

--- javascript.js
+++ javascript.js
@@ -8,18 +8,20 @@ form.addEventListener('submit', function (event) {
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let address = document.getElementById('address').value;
-    let age = document.getElementById('age').value;
     let career = document.getElementById('career').value;
     let profile = document.getElementById('profile').value;
     let skills = document.getElementById('skills').value;
-    let employmentHistory = document.getElementById('employmentHistory').value;
-    let cellphone = document.getElementById('cellphone').value;
-    let firstdate = document.getElementById('firstdate').value;
-    let dateto = document.getElementById('dateto').value;
-    let language = document.getElementById('language').value;
-    let first_education_date = document.getElementById('first_education_date').value;
-    let education_date_to = document.getElementById('education_date_to').value;
-    let education = document.getElementById('education').value;
+     let cellphone = document.getElementById('cellphone').value;
+     let language = document.getElementById('language').value;
+      let employmentHistory = document.getElementById('employmentHistory').value;
+      let first_date = document.getElementById('first_date').value;
+      let date_to = document.getElementById('date_to').value;
+      let education = document.getElementById('education').value;
+      let first_education_date = document.getElementById('first_education_date').value;
+      let education_date_to = document.getElementById('education_date_to').value;
+  
+     
+      
       
 
 
@@ -29,27 +31,20 @@ form.addEventListener('submit', function (event) {
         name: name,
         email: email,
         address: address,
-        age: age,
         career: career,
         profile: profile,
         skills: skills,
-        employmentHistory: employmentHistory,
-        cellphone: cellphone,
-        firstdate: firstdate,
-        firstdateType: 'date',
-        dateto : dateto,
-        datetoType : 'date',
         language: language,
+        employmentHistory: employmentHistory,
+        first_date: first_date,
+        date_to: date_to,
+        education: education,
         first_education_date: first_education_date,
-        education_date_to: education_date_to,
-        education:education
+        education_date_to: education_date_to
 
 
     });
 
-   
-      
- 
     // Redirect to the next page with the data
     window.location.href = `review.html?${params.toString()}`;
 });
